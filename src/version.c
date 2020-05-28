2806e9f5857262427bff8d83537bc34b14c12465
liaotonglang <liaotonglang@gmail.com>
2020-05-28 13:23:01 +0800

    delete mysql-connector-cpp

--- src/version.c
+++ src/version.c
@@ -1,5 +1,5 @@
 // This file is generated automatically, all edit will be ignore.
 
 // version of program, content is the output of git describe --tags
-const char* git_version = "v0.0.1-2-g95ab3e2";
+const char* git_version = "N/A";
 
